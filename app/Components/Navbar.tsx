f686485102c77d16bbf0e967ac44cdaffdf6585a
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2024-03-29 17:48:05 +0530

    "Initial Commit"

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { github } from "../utils/icons";
+import { useRouter } from "next/navigation";
+import { ThemeDropdown } from "./ThemeDropdown/ThemeDropdown";
+import SearchDialog from "./SearchDialog/SearchDialog";
+
+function Navbar() {
+  const router = useRouter();
+  return (
+    <div className="w-full py-4 flex items-center justify-between">
+      <div className="left"></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <SearchDialog />
+        <div className="btn-group flex items-center gap-2">
+          <ThemeDropdown />
+          <Button
+            variant="destructive"
+            className="search-code flex items-center gap-2 border rounded"
+            onClick={() => {
+              router.push("https://github.com/mridulchourasiya/wether_app");
+            }}
+          >
+            {github}
+            Sourace Code
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
